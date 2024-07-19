8c3f7b09d620106885c5ae9a183902671c21dea6
BOTFloindil <floindil@gmail.com>
2024-07-19 10:24:54 +0000

    Automated Javadoc deployment

--- docs/member-search-index.js
+++ docs/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Main","l":"Main()","u":"%3Cinit%3E()"}];updateSearchResults();
+memberSearchIndex = [{"p":"src","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"src","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"src","l":"Main"}];updateSearchResults();
