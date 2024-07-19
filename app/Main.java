00a5e2e368a6d710fc8f1a8ba3bcde98ea2ec08b
Celin Villars <cvillars@arttence.com>
2024-07-19 11:55:18 +0200

    setup

---
+++ app/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.SwingUtilities;
+
+/**
+ * Main class. Serves as the entry Point to the app.
+ */
+public class Main {
+    
+    /**
+     * Not yet defined
+     */
+    public Main() {
+        //empty
+    }
+
+    /**
+     * Executabel for main class.
+     * @param args  None
+     */
+    public static void main(String args[]) {
+        SwingUtilities.invokeLater(Main::new);
+    }
+}
