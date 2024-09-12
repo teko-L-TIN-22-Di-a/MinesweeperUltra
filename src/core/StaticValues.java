b0498b6c0aabc338ea6215e8f035dcc503e4d4ff
Celin Villars <villarscelin@gmail.com>
2024-09-12 20:54:22 +0200

    added difficulties

--- src/core/StaticValues.java
+++ src/core/StaticValues.java
@@ -1,6 +1,7 @@
 package src.core;
 
 import java.awt.Color;
+import java.awt.Point;
 
 /**
  * Dataclass to store fixed values
@@ -25,6 +26,14 @@ public class StaticValues {
     public static int FIELDSIZE = 30;
     /** Amount of tickts for truesight activity */
     public static int TRUESIGHTCOUNTER = 2;
+    /** Size if a small Minefield */
+    public static Point SMALL = new Point(10, 10);
+    /** Size if a medium Minefield */
+    public static Point MEDIUM = new Point(20, 20);
+    /** Size if a large Minefield */
+    public static Point LARGE = new Point(30, 30);
+    /** Amount of Mines for the Minefield sizes for easy, medium and hard difficulties */
+    public static int[][] DIFFICULTY = {{10,20,30},{40,80,120},{90,180,270}};
     /**
      * Colors used for the Fields. The sequence is relevant and
      * the index corresponds to the Field value.
--- src/scenes/Menu.java
+++ src/scenes/Menu.java
@@ -1,6 +1,7 @@
 package src.scenes;
 
 import java.awt.Color;
+import java.awt.Point;
 
 import javax.sound.sampled.Clip;
 
@@ -8,6 +9,7 @@ import src.core.StaticValues;
 import src.assets.Loader;
 import src.assets.SoundMapping;
 import src.components.Button;
+import src.components.Textfield;
 
 /**
  * Creates the menu screen.
@@ -16,6 +18,11 @@ import src.components.Button;
  * @see Scene
  */
 public class Menu extends Scene {
+
+    private Point gridSize;
+    private int mines, difficulty;
+    private String sizeText, difficultyText;
+    private Textfield sizeTextfield, difficultyTextfield;
     /**
      * Sets up the Menu scene.
      */
@@ -23,21 +30,107 @@ public class Menu extends Scene {
         super(true, "menu");
         int width = StaticValues.CANVAS_WIDTH;
         int height = StaticValues.CANVAS_HEIGHT;
+        gridSize = StaticValues.SMALL;
+        difficulty = 0;
+        mines = StaticValues.DIFFICULTY[0][difficulty];
         setBGM(SoundMapping.MENU);
+        sizeText = "SMALL";
+        difficultyText = "EASY";
+        
+        sizeTextfield = new Textfield(width/2-150, height/2-200, "SIZE: " + sizeText);
+        sizeTextfield.setColor(Color.WHITE);
+        registerComponent(sizeTextfield);
 
-        Clip buttonSound = Loader.loadSound(SoundMapping.BUTTON); 
-        Button start = new Button(100, 50, width/2-50, height/2 - 100, "START", Color.GRAY);
+        difficultyTextfield = new Textfield(width/2+50, height/2-200, "DIFFICULTIY: " + difficultyText);
+        difficultyTextfield.setColor(Color.WHITE);
+        registerComponent(difficultyTextfield);
+
+        Clip buttonSound = Loader.loadSound(SoundMapping.BUTTON);
+
+        Button start = new Button(100, 50, width/2-50, height/2+300, "START", Color.GRAY);
         start.setSound(buttonSound);
         start.setAction(() -> {
-            MineField mf = new MineField(30, 30, 200);
+            MineField mf = new MineField(gridSize.x, gridSize.y, mines);
             setNewScene(mf);
         });
-        Button exit = new Button(100, 50, width/2-50, height/2 + 100, "EXIT", Color.GRAY);
+        registerButton(start);
+
+        Button exit = new Button(100, 50, width-125, 25, "EXIT", Color.GRAY);
         exit.setSound(buttonSound);
         exit.setAction(() -> {
             System.exit(0);
         });
-        registerButton(start);
         registerButton(exit);
+
+        Button small = new Button(100, 50, width/2-150, height/2-150, "SMALL", Color.GRAY);
+        small.setSound(buttonSound);
+        small.setAction(() -> {
+            gridSize = StaticValues.SMALL;
+            sizeText = "SMALL";
+            setMines();
+        });
+        registerButton(small);
+
+        Button medium = new Button(100, 50, width/2-150, height/2-25, "MEDIUM", Color.GRAY);
+        medium.setSound(buttonSound);
+        medium.setAction(() -> {
+            gridSize = StaticValues.MEDIUM;
+            sizeText = "MEDIUM";
+            setMines();
+        });
+        registerButton(medium);
+
+        Button large = new Button(100, 50, width/2-150, height/2+100, "LARGE", Color.GRAY);
+        large.setSound(buttonSound);
+        large.setAction(() -> {
+            gridSize = StaticValues.LARGE;
+            sizeText = "LARGE";
+            setMines();
+        });
+        registerButton(large);
+
+        Button easy = new Button(100, 50, width/2+50, height/2-150, "EASY", Color.GRAY);
+        easy.setSound(buttonSound);
+        easy.setAction(() -> {
+            difficulty = 0;
+            difficultyText = "EASY";
+            setMines();
+        });
+        registerButton(easy);
+
+        Button intermediate = new Button(100, 50, width/2+50, height/2-25, "MEDIUM", Color.GRAY);
+        intermediate.setSound(buttonSound);
+        intermediate.setAction(() -> {
+            difficulty = 1;
+            difficultyText = "MEDIUM";
+            setMines();
+        });
+        registerButton(intermediate);
+
+        Button hard = new Button(100, 50, width/2+50, height/2+100, "HARD", Color.GRAY);
+        hard.setSound(buttonSound);
+        hard.setAction(() -> {
+            difficulty = 2;
+            difficultyText = "HARD";
+            setMines();
+        });
+        registerButton(hard);
+    }
+
+    private void setMines() {
+        int size = 0;
+        if (gridSize==StaticValues.MEDIUM) {
+            size = 1;
+        }
+        else if (gridSize==StaticValues.LARGE) {
+            size = 2;
+        }
+        mines = StaticValues.DIFFICULTY[size][difficulty];
+        updateText();
+    }
+
+    private void updateText() {
+        sizeTextfield.setText(("SIZE: " + sizeText));
+        difficultyTextfield.setText("DIFFICULTIY: " + difficultyText);
     }
 }
